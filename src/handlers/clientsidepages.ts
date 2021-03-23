cb77197130574e94552d0b3ba6cb5f212f8b3dfd
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-23 21:22:00 +0100

    fix: fix for client side pages controls

--- src/handlers/clientsidepages.ts
+++ src/handlers/clientsidepages.ts
@@ -1,15 +1,12 @@
-import { IClientSidePage } from '../schema'
-import { HandlerBase } from './handlerbase'
 import {
-  Web,
-  ClientSideWebpart,
-  ClientSidePageComponent,
-  ClientSidePage
+  ClientSidePage, ClientSidePageComponent, ClientSideWebpart, Web
 } from '@pnp/sp'
-import { ProvisioningContext } from '../provisioningcontext'
 import { IProvisioningConfig } from '../provisioningconfig'
-import { TokenHelper } from '../util/tokenhelper'
+import { ProvisioningContext } from '../provisioningcontext'
+import { IClientSidePage } from '../schema'
 import { replaceUrlTokens } from '../util'
+import { TokenHelper } from '../util/tokenhelper'
+import { HandlerBase } from './handlerbase'
 
 /**
  * Describes the Composed Look Object Handler
@@ -34,7 +31,7 @@ export class ClientSidePages extends HandlerBase {
   public async ProvisionObjects(
     web: Web,
     clientSidePages: IClientSidePage[],
-    context: ProvisioningContext
+    context?: ProvisioningContext
   ): Promise<void> {
     this.tokenHelper = new TokenHelper(context, this.config)
     super.scope_started()
@@ -87,39 +84,39 @@ export class ClientSidePages extends HandlerBase {
             c.Id.toLowerCase().includes(control.Id.toLowerCase())
           )
           if (!partDef) {
-            try {
-              let properties = this.tokenHelper.replaceTokens(
-                JSON.stringify(control.Properties)
-              )
-              properties = replaceUrlTokens(properties, this.config)
-              const part = ClientSideWebpart.fromComponentDef(
-                partDef
-              ).setProperties<any>(JSON.parse(properties))
-              if (control.ServerProcessedContent) {
-                const serverProcessedContent = this.tokenHelper.replaceTokens(
-                  JSON.stringify(control.ServerProcessedContent)
-                )
-                part.data.webPartData.serverProcessedContent = JSON.parse(
-                  serverProcessedContent
-                )
-              }
-              super.log_info(
-                'processClientSidePage',
-                `Adding ${partDef.Name} to client side page ${clientSidePage.Name}`
-              )
-              column.addControl(part)
-            } catch (error) {
-              // eslint-disable-next-line no-console
-              console.log(error)
-              super.log_info(
-                'processClientSidePage',
-                `Failed adding part ${partDef.Name} to client side page ${clientSidePage.Name}`
-              )
-            }
-          } else {
             super.log_warn(
               'processClientSidePage',
               `Client side web part with definition id ${control.Id} not found.`
+            )
+            continue
+          }
+          try {
+            let properties = this.tokenHelper.replaceTokens(
+              JSON.stringify(control.Properties)
+            )
+            properties = replaceUrlTokens(properties, this.config)
+            const part = ClientSideWebpart.fromComponentDef(
+              partDef
+            ).setProperties<any>(JSON.parse(properties))
+            if (control.ServerProcessedContent) {
+              const serverProcessedContent = this.tokenHelper.replaceTokens(
+                JSON.stringify(control.ServerProcessedContent)
+              )
+              part.data.webPartData.serverProcessedContent = JSON.parse(
+                serverProcessedContent
+              )
+            }
+            super.log_info(
+              'processClientSidePage',
+              `Adding ${partDef.Name} to client side page ${clientSidePage.Name}`
+            )
+            column.addControl(part)
+          } catch (error) {
+            // eslint-disable-next-line no-console
+            console.log(error)
+            super.log_info(
+              'processClientSidePage',
+              `Failed adding part ${partDef.Name} to client side page ${clientSidePage.Name}`
             )
           }
         }
